6ba81c9b61ecdb9e853abfc079db8076e7bc4578
Dustin Hebgen <d.hebgen@ecc-online.net>
2020-11-25 16:19:01 +0100

    changed server.js

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
-import express from 'express';
-import serveStatic from 'serve-static';
-import path from 'path';
-
-const app = express();
+const express = require('express');
+const serveStatic = require('serve-static');
+const path = require('path');
+app = express();
 app.use(serveStatic(path.join(__dirname, 'dist')));
 const port = process.env.PORT || 80;
 app.listen(port);
